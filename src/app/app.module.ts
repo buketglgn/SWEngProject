41a492747306fa9b424d4e4ebb6bc09a1373a46e
Rümeysa <rumeysakaragoz18@hotmail.com>
2021-12-08 21:54:55 +0300

    login register done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,12 @@ import { LeftMenuComponent } from './components/left-menu/left-menu.component';
 import { NaviComponent } from './components/navi/navi.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './login/login.component';
+import { FlexLayoutModule } from '@angular/flex-layout';import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule} from '@angular/material/input';
+import { MatButtonModule} from '@angular/material/button';
+import { MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { RegisterComponent } from './register/register.component';
 
 
 
@@ -17,12 +23,19 @@ import { LoginComponent } from './login/login.component';
     NaviComponent,
     LeftMenuComponent,
     LoginComponent,
+    RegisterComponent
    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    FlexLayoutModule,
+    MatToolbarModule
     
   ],
   providers: [],
